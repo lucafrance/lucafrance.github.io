77886eff86bb53bedd0590e11a15758ec763fa80
Luca Franceschini <luca.france@outlook.com>
2020-11-13 16:19:53 +0100

    Added script to update the stories from medium

---
+++ update_medium_stories.ps1
@@ -0,0 +1,3 @@
+./venv/Scripts/Activate.ps1
+python -m pip install --upgrade -q pip mediumbackup
+python -m mediumbackup "lucafrance" --f "md" --backup_dir "_posts" --download_images --images_dir "/assets/medium_images"
