7a136c01fd4ee77ceab36f23057ca8532c118426
Luca Franceschini <luca.france@outlook.com>
2021-03-29 16:03:18 +0200

    Removed no longer necessary scripts

--- move_to_folder.ps1
+++
@@ -1,4 +0,0 @@
-# Move to current directory https://stackoverflow.com/a/4724421
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-cd $dir
--- update_medium_stories.ps1
+++
@@ -1,3 +0,0 @@
-./venv/Scripts/Activate.ps1
-python -m pip install --upgrade -q pip mediumbackup
-python -m mediumbackup "lucafrance" --f "md" --backup_dir "_posts" --download_images --images_dir "/assets/medium_images" --jekyll_front_matter
